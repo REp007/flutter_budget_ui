14252182649fd49a26f0c80facf5c9c06ac4b8af
REp007 <elamraniy292@gmail.com>
2025-03-10 23:23:40 +0100

    Implement home screen with weekly spending bar chart and data models

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/data/data.dart';
+// widgets
+import 'package:flutter_budget_ui/widgets/bar_chart.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            forceElevated: false,
+            // floating true; this let you when scroll up to show the AppBar
+            floating: true,
+            expandedHeight: 100.0,
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(
+                'Simple Budget',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.all(10.0),
+                  /**
+                   * Error when specif height 100.0
+                   */
+                  // height: 100.0,
+                  // color: Colors.red,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(0, 2),
+                          blurRadius: 6.0),
+                    ],
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: BarChart(expenses: weeklySpending),
+                );
+              },
+              childCount: 1,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
