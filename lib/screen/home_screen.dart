7e87a155f20dc3e58d9d733ebf291bbab08ad5be
REp007 <elamraniy292@gmail.com>
2025-03-11 22:40:31 +0100

    Refactor home screen category display for improved layout and readability

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_budget_ui/data/data.dart';
 import 'package:flutter_budget_ui/models/category_model.dart';
-import 'package:flutter_budget_ui/models/expense_model.dart';
+// import 'package:flutter_budget_ui/models/expense_model.dart';
 // widgets
 import 'package:flutter_budget_ui/widgets/bar_chart.dart';
 
@@ -63,9 +63,9 @@ class _HomeScreenState extends State<HomeScreen> {
                 } else {
                   final Category category = categories[index - 1];
                   double totalAmountSpent = 0;
-                  category.expenses.forEach((Expense exp) {
+                  for (var exp in category.expenses) {
                     totalAmountSpent += exp.cost;
-                  });
+                  }
                   return _buildCategory(category, totalAmountSpent);
                 }
               },
@@ -80,40 +80,37 @@ class _HomeScreenState extends State<HomeScreen> {
   // make _buildCategory func
   _buildCategory(Category category, double totalAmountSpent) {
     return Container(
-        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        height: 100.0,
-        width: double.infinity,
-        // color: Colors.red,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black12, offset: Offset(0, 2), blurRadius: 6.0),
-            ]),
-        child: Padding(
-          padding: EdgeInsets.all(12.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      padding: EdgeInsets.all(20.0),
+      height: 100.0,
+      width: double.infinity,
+      // color: Colors.red,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12, offset: Offset(0, 2), blurRadius: 6.0),
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    category.name,
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                    '\$${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / \$${category.maxAmount.toStringAsFixed(2)}',
-                    style:
-                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
-                  )
-                ],
+              Text(
+                category.name,
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
               ),
-              Row()
+              Text(
+                '\$${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / \$${category.maxAmount.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+              )
             ],
           ),
-        ));
+          Row()
+        ],
+      ),
+    );
   }
 }
