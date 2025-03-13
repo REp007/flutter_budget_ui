102866993f6c1105ba0a13d076eefe9d1b3203d7
REp007 <elamraniy292@gmail.com>
2025-03-13 01:57:39 +0100

    Add expense list display to Category Screen

--- lib/screen/category_screen.dart
+++ lib/screen/category_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_budget_ui/helpers/color_helper.dart';
 import 'package:flutter_budget_ui/models/category_model.dart';
+import 'package:flutter_budget_ui/models/expense_model.dart';
 import 'package:flutter_budget_ui/widgets/radial_painter.dart';
 
 class CategoryScreen extends StatefulWidget {
@@ -12,6 +13,47 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  _buildExpenses() {
+    List<Widget> expenseList = [];
+    for (var expense in widget.category.expenses) {
+      expenseList.add(Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        height: 80.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black12, offset: Offset(0, 2), blurRadius: 6.0)
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                expense.name,
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '-\$${expense.cost.toStringAsFixed(2)}',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.red),
+              )
+            ],
+          ),
+        ),
+      ));
+    }
+    return Column(
+      children: expenseList,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     /**
@@ -73,7 +115,8 @@ class _CategoryScreenState extends State<CategoryScreen> {
                   ),
                 ),
               ),
-            )
+            ),
+            _buildExpenses()
           ],
         ),
       ),
